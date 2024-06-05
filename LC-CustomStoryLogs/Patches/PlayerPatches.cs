b5b75b086df05fcca547192580121d39b1dc47b3
Yorimor <liamcrane01@gmail.com>
2024-06-05 20:10:04 +0100

    Start of custom placement tool Borrowed/inspired by https://github.com/Kitseru/StarshipDeliveryMod

---
+++ LC-CustomStoryLogs/Patches/PlayerPatches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomStoryLogs.Tools;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CustomStoryLogs.Patches;
+
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal class PlayerControllerBPatch
+{
+    [HarmonyPatch("ConnectClientToPlayerObject")]
+    [HarmonyPostfix]
+    public static void ConnectClientToPlayerObjectPatch(ref PlayerControllerB __instance)
+    {
+        if(__instance.IsOwner)
+        {
+            __instance.gameObject.AddComponent<LogPlacementTool>();
+        }
+    }
+}
---
+++ LC-CustomStoryLogs/Tools/LogPlacementTool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using GameNetcodeStuff;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace CustomStoryLogs.Tools;
+
+public class LogPlacementTool: MonoBehaviour
+{
+    public InputAction activateToolAction;
+    
+    private GameObject dummyLogPrefab;
+    private GameObject dummyLog = null;
+    
+    private GameObject canvasPrefab;
+    
+    private void Start()
+    {
+        activateToolAction = KeyBindManager.CreateInputAction("ActivateLogPositionTool", "<Keyboard>/p", "press");
+        
+        dummyLogPrefab = CustomStoryLogs.MyAssets.LoadAsset<GameObject>("assets/yorimor/customstorylogs/logpreview.prefab");
+        canvasPrefab = CustomStoryLogs.MyAssets.LoadAsset<GameObject>("assets/yorimor/customstorylogs/canvas.prefab");
+    }
+
+    void Update()
+    {
+        if (activateToolAction.triggered)
+        {
+            ActivateTool();
+        }
+    }
+
+    void ActivateTool()
+    {
+        CustomStoryLogs.Logger.LogInfo("ToggleTool");
+    }
+}
